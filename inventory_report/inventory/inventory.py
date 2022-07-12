791c490947a22251ad027b29b859d247c3c4631f
Flávio Bianchetti <fjb.developer@gmail.com>
2022-07-12 19:15:10 -0300

    feat(Inventory): adaptada classe para gerar inclusive relatórios XML
    Detalhes:
    - Recebe um caminho para o arquivo CSV, JSON ou XML e o tipo de relatório
    a ser gerado, retornando o relatório solicitado.
    
    Requisito 7 atendido.

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+from xml.etree import cElementTree as ET
 
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
@@ -17,12 +18,13 @@ class Inventory(CompleteReport, SimpleReport):
 
     def get_file_data(caminho):
         extensao = caminho.split(".")[-1]
-        print(extensao.upper())
         resultado = ""
         if extensao.upper() == "CSV":
             resultado = Inventory.get_file_csv(caminho)
         if extensao.upper() == "JSON":
             resultado = Inventory.get_file_json(caminho)
+        if extensao.upper() == "XML":
+            resultado = Inventory.get_file_xml(caminho)
         return resultado
 
     def get_file_csv(caminho):
@@ -47,6 +49,36 @@ class Inventory(CompleteReport, SimpleReport):
                 dados = json.load(file)
                 for row in dados:
                     lista_de_produtos.append(row)
+                return lista_de_produtos
+        except IOError:
+            print("Error: o arquivo não existe.")
+            return
+        except Exception:
+            print("Error: dados inválidos.")
+            return
+
+    # https://docs.python.org/pt-br/3/library/xml.etree.elementtree.html?highlight=xml#xml.etree.ElementTree.XML
+    def get_file_xml(caminho):
+        try:
+            lista_de_produtos = []
+            with open(caminho) as file:
+                tree = ET.parse(file)
+                dados = tree.getroot()
+                for row in dados.findall("record"):
+                    produto = {
+                        "id": int(row.find("id").text),
+                        "nome_do_produto": row.find("nome_do_produto").text,
+                        "nome_da_empresa": row.find("nome_da_empresa").text,
+                        "data_de_fabricacao": row.find(
+                            "data_de_fabricacao"
+                        ).text,
+                        "data_de_validade": row.find("data_de_validade").text,
+                        "numero_de_serie": row.find("numero_de_serie").text,
+                        "instrucoes_de_armazenamento": row.find(
+                            "instrucoes_de_armazenamento"
+                        ).text,
+                    }
+                    lista_de_produtos.append(produto)
                 return lista_de_produtos
         except IOError:
             print("Error: o arquivo não existe.")
