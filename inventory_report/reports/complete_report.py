0bfdfa604316f3c96e45820d099da76882223c19
Flávio Bianchetti <fjb.developer@gmail.com>
2022-07-12 11:47:21 -0300

    feat(CompleteReport): incluída classe para gerar a versão completa do relatório
    Detalhes:
    - O relatório completo retorna, além das informações do relatório simples, a quan-
    tidade de produtos estocados por empresa.
    
    Requisito 4 atendido.

---
+++ inventory_report/reports/complete_report.py
@@ -0,0 +1,15 @@
+# https://stackoverflow.com/questions/36807701/how-to-get-the-python-counter-output-ordered-by-order-of-inputs
+from collections import OrderedDict, Counter
+
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport, OrderedDict):
+    def generate(list):
+        resultado = SimpleReport.generate(list)
+        empresas = Counter([item["nome_da_empresa"] for item in list])
+        resultado += "\nProdutos estocados por empresa:\n"
+        for key, value in empresas.items():
+            resultado += f"- {key}: {value}\n"
+        print(resultado)
+        return resultado
